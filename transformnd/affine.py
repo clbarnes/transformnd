247b740558fcd170cf67b716c1b0fa04ad7cf528
Chris Barnes <barnesc@janelia.hhmi.org>
2021-05-11 19:21:46 +0100

    Document affine

--- transformnd/affine.py
+++ transformnd/affine.py
@@ -28,6 +28,23 @@ class AffineTransform(Transform):
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
     ):
+        """Affine transformation using an augmented matrix.
+
+        Matrix must have shape (D+1, D+1) or (D, D+1);
+        the bottom row is assumed to be [0, 0, ..., 0, 1].
+
+        Parameters
+        ----------
+        matrix : ArrayLike
+            Affine transformation matrix.
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Raises
+        ------
+        ValueError
+            Malformed matrix.
+        """
         super().__init__(source_space=source_space, target_space=target_space)
         m = np.asarray(matrix)
 
@@ -60,6 +77,21 @@ class AffineTransform(Transform):
         )
 
     def __matmul__(self, rhs: AffineTransform) -> AffineTransform:
+        """Compose two affine transforms by matrix multiplication.
+
+        Parameters
+        ----------
+        rhs : AffineTransform
+
+        Returns
+        -------
+        AffineTransform
+
+        Raises
+        ------
+        ValueError
+            Incompatible transforms.
+        """
         if not isinstance(rhs, AffineTransform):
             return NotImplemented
         if self.matrix.shape != rhs.matrix.shape:
@@ -78,19 +110,34 @@ class AffineTransform(Transform):
     def from_linear_map(
         cls,
         linear_map: ArrayLike,
-        translation=None,
+        translation=0,
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
     ):
+        """Create an augmented affine matrix from a linear map,
+        with an optional translation.
+
+        Parameters
+        ----------
+        linear_map : ArrayLike
+            Shape (D, D)
+        translation : ArrayLike, optional
+            Translation to add to the matrix, by default None
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        [type]
+            [description]
+        """
         lin_map = np.asarray(linear_map)
 
         side = len(lin_map) + 1
         matrix = np.eye(shape=(side, side), dtype=lin_map.dtype)
         matrix[:-1, :] = lin_map
-
-        if translation is not None:
-            matrix[:-1, -1] = translation
+        matrix[:-1, -1] = translation
 
         return cls(matrix, source_space=source_space, target_space=target_space)
 
@@ -101,7 +148,20 @@ class AffineTransform(Transform):
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
-    ):
+    ) -> AffineTransform:
+        """Create an identity affine transformation.
+
+        Parameters
+        ----------
+        ndim : int
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        [type]
+            [description]
+        """
         return cls(
             np.eye(ndim + 1), source_space=source_space, target_space=target_space
         )
@@ -110,11 +170,26 @@ class AffineTransform(Transform):
     def translation(
         cls,
         translation: ArrayLike,
-        ndim=None,
+        ndim: Optional[int] = None,
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
-    ):
+    ) -> AffineTransform:
+        """Create an affine translation.
+
+        Parameters
+        ----------
+        translation : ArrayLike
+            If scalar, broadcast to ndim.
+        ndim : int, optional
+            If translation is scalar, how many dims to use.
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        AffineTransform
+        """
         t = arg_as_array(translation, ndim)
         m = np.eye(len(t) + 1, dtype=t.dtype)
         m[:-1, -1] = t
@@ -125,11 +200,27 @@ class AffineTransform(Transform):
     def scaling(
         cls,
         scale: ArrayLike,
-        ndim=None,
+        ndim: Optional[int] = None,
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
-    ):
+    ) -> AffineTransform:
+        """Create an affine scaling.
+
+        Parameters
+        ----------
+        scale : ArrayLike
+            If scalar, broadcast to ndim.
+        ndim : Optional[int], optional
+            If scale is scalar, how many dimensions to use
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        AffineTransform
+            [description]
+        """
         s = arg_as_array(scale, ndim)
         m = np.eye(len(s) + 1, dtype=s.dtype)
         m[:-1, :-1] *= s
@@ -144,7 +235,22 @@ class AffineTransform(Transform):
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
-    ):
+    ) -> AffineTransform:
+        """Create an affine reflection.
+
+        Parameters
+        ----------
+        axis : Union[int, Container[int]]
+            A single axis or multiple to reflect in.
+        ndim : int
+            How many dimensions to work in.
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        AffineTransform
+        """
         if np.isscalar(axis):
             axis = [axis]
         values = [-1 if idx in axis else 1 for idx in range(ndim)]
@@ -162,7 +268,24 @@ class AffineTransform(Transform):
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
-    ):
+    ) -> AffineTransform:
+        """Create a 2D affine rotation.
+
+        Parameters
+        ----------
+        rotation : float
+            Angle to rotate.
+        degrees : bool, optional
+            Whether rotation is in degrees (rather than radians), by default True
+        clockwise : bool, optional
+            Whether rotation is clockwise, by default False
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        AffineTransform
+        """
         if degrees:
             rotation = np.deg2rad(rotation)
         if clockwise:
@@ -187,7 +310,31 @@ class AffineTransform(Transform):
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
-    ):
+    ) -> AffineTransform:
+        """Create a 3D affine rotation.
+
+        Parameters
+        ----------
+        rotation : Union[float, Tuple[float, float, float]]
+            Either a single rotation for all axes, or 1 for each.
+        degrees : bool, optional
+            Whether rotation is in degrees (rather than radians), by default True
+        clockwise : bool, optional
+            Whether rotation is clockwise, by default False
+        order : tuple, optional
+            What order to apply the rotations, by default (0, 1, 2)
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        AffineTransform
+
+        Raises
+        ------
+        ValueError
+            Incompatible order.
+        """
         if np.isscalar(rotation):
             r = np.array([rotation] * 3)
         else:
@@ -240,11 +387,38 @@ class AffineTransform(Transform):
     def shearing(
         cls,
         factor: Union[float, np.ndarray],
-        ndim=None,
+        ndim: Optional[int] = None,
         *,
         source_space: Optional[SpaceRef] = None,
         target_space: Optional[SpaceRef] = None,
-    ):
+    ) -> AffineTransform:
+        """Create an affine shear.
+
+        ``factor`` can be a scalar to broadcast to all dimensions,
+        or a D-length list of D-1 lists.
+        The first inner list contains the shear factors in the first dimension
+        for all *but* the first dimension.
+        The second inner list contains the shear factors in the second dimension
+        for all the *but* the second dimension, etc.
+
+        Parameters
+        ----------
+        factor : Union[float, np.ndarray]
+            Shear scale factors; see above for more details.
+        ndim : Optional[int], optional
+            If factor is scalar, broadcast to this many dimensions, by default None
+        source_space : Optional[SpaceRef], optional
+        target_space : Optional[SpaceRef], optional
+
+        Returns
+        -------
+        AffineTransform
+
+        Raises
+        ------
+        ValueError
+            Incompatible factor.
+        """
         if np.isscalar(factor):
             if ndim is None:
                 raise ValueError("If factor is scalar, ndim must be defined")
