d1b1b406d8359efde3181040435fa10de8f686e6
Chris Barnes <chrislloydbarnes@gmail.com>
2026-04-15 13:39:25 +0200

    Revert "map_axis init + test"

--- src/transformnd/transforms/__init__.py
+++ src/transformnd/transforms/__init__.py
@@ -3,6 +3,5 @@
 from .affine import Affine
 from .reflection import Reflect
 from .simple import Identity, Scale, Translate
-from .map_axis import MapAxis
 
-__all__ = ["Affine", "Identity", "Reflect", "Scale", "Translate", "MapAxis"]
+__all__ = ["Affine", "Identity", "Reflect", "Scale", "Translate"]
