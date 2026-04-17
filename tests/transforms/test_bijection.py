e841f6177cc71a81850ef1724881a226d8437838
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-04-17 20:18:50 +0200

    rename and fix up tests

--- tests/transforms/test_bijection.py
+++ tests/transforms/test_bijection.py
@@ -1,12 +1,11 @@
 import numpy as np
 from transformnd.transforms import Bijection, Scale, MapAxis
 
-import pytest
 
-
-@pytest.mark.parametrize("scale", [1, 2, 3])
-def test_bijection_apply_scale(scale):
-    coords5x3 = np.random.rand(5, 3)
+def test_bijection_apply_scale():
+    scale = 1.5
+    rng = np.random.default_rng(1991)
+    coords5x3 = rng.random((5, 3))
     forward = Scale(scale)
     inverse = Scale(1 / scale)
     bij = Bijection(forward, inverse)
@@ -14,18 +13,19 @@ def test_bijection_apply_scale(scale):
 
 
 def test_bijection_apply_map_axis():
-    coords2x2 = np.array([[1, 2], [3, 4]])
+    coords2x2 = np.array([[1, 2], [3, 4]], dtype=float)
     forward = MapAxis(permutation=[1, 0])
     inverse = MapAxis(permutation=[1, 0])
     bij = Bijection(forward, inverse)
     coords2x2_inv = bij.apply(coords2x2)
-    assert np.array_equal(coords2x2_inv, np.array([[2, 1], [4, 3]]))
+    assert np.array_equal(coords2x2_inv, np.array([[2, 1], [4, 3]], dtype=float))
     bij_inv = ~bij
     assert np.array_equal(bij_inv.apply(coords2x2_inv), coords2x2)
 
 
 def test_bijection_invert_scale():
-    coords5x3 = np.random.rand(5, 3)
+    rng = np.random.default_rng(1991)
+    coords5x3 = rng.random((5, 3))
     forward = Scale(2)
     inverse = Scale(0.5)
     bij = Bijection(forward, inverse)
@@ -35,7 +35,7 @@ def test_bijection_invert_scale():
 
 def test_bijection_invert_map_axis():
     # [2, 0, 1] is the inverse of [1, 2, 0], not its own inverse
-    coords3x2 = np.array([[1, 2, 3], [4, 5, 6]])
+    coords3x2 = np.array([[1, 2, 3], [4, 5, 6]], dtype=float)
     forward = MapAxis(permutation=[1, 2, 0])
     inverse = MapAxis(permutation=[2, 0, 1])
     bij = Bijection(forward, inverse)
