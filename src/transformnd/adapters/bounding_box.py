d5858c0717d61e183d765f3a11f07b81d63f9c6b
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-04-20 12:46:02 +0100

    bbox adapter typing

--- src/transformnd/adapters/bounding_box.py
+++ src/transformnd/adapters/bounding_box.py
@@ -2,11 +2,12 @@ from array_api_compat import array_namespace
 from transformnd.base import Transform
 from .base import BaseAdapter
 from ..extents.bounding_box import BoundingBox
+from ..base import ArrayT
 
 
-class BoundingBoxAdapter[ArrayT](BaseAdapter[BoundingBox]):
+class BoundingBoxAdapter(BaseAdapter[BoundingBox[ArrayT], ArrayT]):
     def apply(
-        self, transform: Transform, obj: BoundingBox[ArrayT]
+        self, transform: Transform[ArrayT], obj: BoundingBox[ArrayT]
     ) -> BoundingBox[ArrayT]:
         xp = array_namespace(obj.mins)
         stacked = xp.stack([obj.mins, obj.maxes])
