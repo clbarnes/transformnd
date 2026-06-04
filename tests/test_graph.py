c088012a84c9da073401ac2865cb20ec21bf2670
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-06-04 09:08:19 +0100

    minor test fixes

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,17 +4,17 @@ from transformnd.types import Spaces
 from transformnd.transforms.simple import Translate, Scale
 from transformnd.transforms.affine import Affine
 
+import pytest
+
 import numpy as np
+
 
 def test_graph_traversal():
     t1 = Translate([1, 2], spaces=Spaces("A", "B"))
     t2 = Scale([2, 3], spaces=Spaces("B", "C"))
     t3 = Affine(np.eye(3), spaces=Spaces("C", "D"))
 
-    graph = TransformGraph()
-    graph.add_transforms([t1])
-    graph.add_transforms([t2])
-    graph.add_transforms([t3])
+    graph: TransformGraph = TransformGraph([t1, t2, t3])
 
     seq = graph.get_sequence("A", "D")
     assert isinstance(seq, TransformSequence)
@@ -25,7 +25,10 @@ def test_graph_traversal():
     assert simplified_seq.spaces.target == "D"
 
     expected_affine = np.array([[2, 0, 2], [0, 3, 6], [0, 0, 1]])
-    assert np.array_equal(simplified_seq.to_affine().matrix, expected_affine)
+    got_affine = simplified_seq.to_affine()
+    assert got_affine is not None
+    assert got_affine.matrix == pytest.approx(expected_affine)
+
 
 if __name__ == "__main__":
     test_graph_traversal()
