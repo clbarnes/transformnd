a9277f87e2ad2ce8c3535f72299d79eda0ae7c7f
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-07-07 10:29:17 +0100

    More complicated grid example

--- tests/transforms/test_grid.py
+++ tests/transforms/test_grid.py
@@ -1,8 +1,9 @@
 from transformnd.transforms import GridInterpolation
 import pytest
+import numpy as np
 
 
-def test_simple(coords5x3):
-    t = GridInterpolation([lambda x: x * 2, lambda x: x * 3, lambda x: x * 4])
+def test_pow(coords5x3):
+    t = GridInterpolation([lambda x: x**2, lambda x: x**3, lambda x: x**4])
     out = t.apply(coords5x3)
-    assert out == pytest.approx(coords5x3 * [2, 3, 4])
+    assert out == pytest.approx(np.pow(coords5x3, [2, 3, 4]))
